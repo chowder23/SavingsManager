2932577a64934305c3ae0f4f68e01e953d6d8392
chowder23 <benjamin2333@gmail.com>
2019-10-03 19:47:06 +0200

    Created the saving class with basic functions and structure

---
+++ src/Saving.kt
@@ -0,0 +1,35 @@
+import java.time.LocalDateTime
+import java.util.*
+
+class Saving(val name:String, val monthlySavingAmount:Double, val desiredAmmount:Double) {
+    val savingStartDateTime = LocalDateTime.now()
+    private var elapsedMonths:Int = savingStartDateTime.monthValue.toInt() - LocalDateTime.now().monthValue.toInt()
+
+    fun toSave():String{
+        return "$name+|$monthlySavingAmount+|$desiredAmmount+|$savingStartDateTime"
+    }
+
+    fun getElapsedMonths():Int
+    {
+        return elapsedMonths
+    }
+
+    fun getSavingAmount():Double
+    {
+        return monthlySavingAmount*elapsedMonths
+    }
+
+    fun getMonthsToReachGoal():Int
+    {
+        return (desiredAmmount/monthlySavingAmount).toInt()-getElapsedMonths()
+    }
+
+    override fun toString(): String {
+        return "Saving name: $name \n" +
+                "Saving amount: ${getElapsedMonths()*monthlySavingAmount} \n" +
+                "Desired amount: $desiredAmmount \n" +
+                "Start date: $savingStartDateTime" +
+                "Elapsed months: ${getElapsedMonths()}" +
+                "Time to reach goal: ${getMonthsToReachGoal()}"
+    }
+}
