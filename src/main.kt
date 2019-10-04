ce43a9b12f5254722951913ca3209af4f7659e8f
chowder23 <benjamin2333@gmail.com>
2019-10-04 14:45:31 +0200

    minor changes and test in SavingManager class

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,18 @@
 import Saving
+import java.lang.Exception
+
 fun main()
 {
     var testSaving:Saving =Saving("ház",50000.0,50000000.0)
-    println(testSaving.toString())
+    var mySavingManager = SavingManager()
+    try {
+        mySavingManager.addNewSaving(testSaving)
+    }
+    catch (exp:Exception)
+    {
+        println(exp.message)
+    }
+
+
+    //println(testSaving.toString())
 }
