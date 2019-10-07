e0f84c68b60ccaeeafde8132a5545d69242cb095
chowder23 <benjamin2333@gmail.com>
2019-10-07 15:01:45 +0200

    removed the test from the master

--- src/main.kt
+++ src/main.kt
@@ -4,22 +4,5 @@ import java.lang.Exception
 fun main()
 {
 
-    var mySavingManager = SavingManager()
-    try {
-        mySavingManager.InitFileManager("savings.txt")
-        mySavingManager.Load()
-        //mySavingManager.addNewSaving("ház",50000.0,50000000.0)
-        //mySavingManager.addNewSaving("Ház",50000.0,15000000.0)
-        //mySavingManager.Save()
-    }
-    catch (exp:Exception)
-    {
-        println(exp.message)
-    }
-    for (name in mySavingManager.getSavingsNames())
-    {
-        println(name)
-    }
-
-    //println(testSaving.toString())
+   
 }
