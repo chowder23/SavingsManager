d79bc2ee6668ccd7b40d6479183c8992e4dcfe97
chowder23 <benjamin2333@gmail.com>
2019-10-08 08:10:53 +0200

    fixed some minor bug when adding amount to a saving

--- src/Saving.kt
+++ src/Saving.kt
@@ -25,7 +25,7 @@ class Saving(private val savingId:Int, val name:String, val monthlySavingAmount:
 
     fun getMonthsToReachGoal():Int
     {
-        return (desiredAmmount/monthlySavingAmount).toInt()-getElapsedMonths()
+        return ((desiredAmmount-savedAmount)/monthlySavingAmount).toInt()-getElapsedMonths()
     }
 
     fun getId():Int
@@ -35,7 +35,7 @@ class Saving(private val savingId:Int, val name:String, val monthlySavingAmount:
 
     fun addToSavedAmount(amount:Double)
     {
-        savedAmount*=amount
+        savedAmount+=amount
     }
     override fun equals(other: Any?): Boolean {
         val otherSaving = other as Saving
